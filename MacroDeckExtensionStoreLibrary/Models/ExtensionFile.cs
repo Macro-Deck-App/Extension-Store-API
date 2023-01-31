0b6eda7cf8294ad4f0e3dffdefe637b28ef5a712
Manuel Mayer <suchbyte@gmail.com>
2023-01-31 20:58:02 +0100

    Revert changes

--- MacroDeckExtensionStoreLibrary/Models/ExtensionFile.cs
+++ MacroDeckExtensionStoreLibrary/Models/ExtensionFile.cs
@@ -4,6 +4,8 @@ public class ExtensionFile
 {
     public string Version { get; set; }
     public int MinApiVersion { get; set; }
+    public string PackageFileName { get; set; }
+    public string IconFileName { get; set; }
     public string DescriptionHtml { get; set; }
     public string Md5Hash { get; set; }
     public string LicenseName { get; set; }
